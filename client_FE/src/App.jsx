d2010e24304effeb2727630c8efcdce24e9b7bcb
jaynguyen1510 <ngnhatnam1510@gmail.com>
2024-09-17 00:20:10 +0700

    create pages and routes

--- client_FE/src/App.jsx
+++ client_FE/src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Dasboard from './pages/DasboardPage';
+import About from './pages/About';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
+import ProjectPage from './pages/ProjectPage';
+
 export default function App() {
-    return <h1 className="text-3xl text-red-500">App</h1>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />}></Route>
+                <Route path="/dashboard" element={<Dasboard />}></Route>
+                <Route path="/about" element={<About />}></Route>
+                <Route path="/sign-in" element={<SignInPage />}></Route>
+                <Route path="/sign-up" element={<SignUpPage />}></Route>
+                <Route path="/project-page" element={<ProjectPage />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
