f95a14a6b075fae713db25e5bd03c88a3ea2cea8
jaynguyen1510 <ngnhatnam1510@gmail.com>
2024-09-18 00:36:35 +0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,23 @@
 // const express = require('express')
 import express from 'express'
-const app = express();
+import mongoose from 'mongoose';
+import dotenv from "dotenv"
 
-app.listen(3000, () => {
-    console.log("listening on");
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_DB)
+
+    .then(() => {
+        console.log("Connect MongoDB successfully");
+
+    })
+    .catch((error) => {
+        console.log("Error connecting to DB", error);
+    });
+
+const app = express();
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+    console.log("server running on", port);
 
 })
