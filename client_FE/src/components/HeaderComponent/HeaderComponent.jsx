cc78003a9c1e847b1a71e5bfbe87c26ceb8c2323
jaynguyen1510 <ngnhatnam1510@gmail.com>
2024-09-17 13:56:37 +0700

    creat the header ssection

---
+++ client_FE/src/components/HeaderComponent/HeaderComponent.jsx
@@ -0,0 +1,63 @@
+import { Button, Navbar, TextInput } from 'flowbite-react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon } from 'react-icons/fa';
+
+const HeaderComponent = () => {
+    const navigate = useNavigate();
+    const path = useLocation().pathname;
+    console.log('Loading', path);
+
+    return (
+        <Navbar className="border-b-2">
+            <div
+                onClick={() => navigate('/')}
+                className="cursor-pointer self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+            >
+                <span className="px-1 pt-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                    Sức khỏe và thể hình
+                </span>
+                Blog
+            </div>
+            <form>
+                <TextInput
+                    type="text"
+                    placeholder="Tìm kiếm"
+                    rightIcon={AiOutlineSearch}
+                    className="hidden lg:inline"
+                />
+            </form>
+            <Button className="w-12 h-10 lg:hidden " color="gray" pill>
+                <AiOutlineSearch />
+            </Button>
+            <div className="flex gap-2 md:order-2">
+                <Button className="w-12 h-10 hidden sm:inline" color={'gray'} pill>
+                    <FaMoon />
+                </Button>
+                <Button gradientDuoTone="purpleToBlue" onClick={() => navigate('/sign-in')}>
+                    Đăng nhập
+                </Button>
+                <Navbar.Toggle />
+            </div>
+            <Navbar.Collapse>
+                <Navbar.Link active={path === '/'}>
+                    <div onClick={() => navigate('/')} className="cursor-pointer">
+                        Trang chủ
+                    </div>
+                </Navbar.Link>
+                <Navbar.Link active={path === '/about'}>
+                    <div onClick={() => navigate('/about')} className="cursor-pointer">
+                        Tổng quan
+                    </div>
+                </Navbar.Link>
+                <Navbar.Link active={path === '/project-page'}>
+                    <div onClick={() => navigate('/project-page')} className="cursor-pointer">
+                        Lịch tập
+                    </div>
+                </Navbar.Link>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+export default HeaderComponent;
--- client_FE/src/routers/App.jsx
+++ client_FE/src/routers/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import Dasboard from './pages/DasboardPage';
-import About from './pages/About';
-import SignInPage from './pages/SignInPage';
-import SignUpPage from './pages/SignUpPage';
-import ProjectPage from './pages/ProjectPage';
+import HomePage from './../pages/HomePage';
+import Dasboard from './../pages/DasboardPage';
+import About from './../pages/About';
+import SignInPage from './../pages/SignInPage';
+import SignUpPage from './../pages/SignUpPage';
+import ProjectPage from './../pages/ProjectPage';
+import HeaderComponent from '../components/HeaderComponent/HeaderComponent';
 
 export default function App() {
     return (
         <BrowserRouter>
+            <HeaderComponent />
             <Routes>
                 <Route path="/" element={<HomePage />}></Route>
                 <Route path="/dashboard" element={<Dasboard />}></Route>
