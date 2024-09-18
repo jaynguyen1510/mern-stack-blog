ee9678f72743f658b616766bb6a361314b01cf4f
jaynguyen1510 <ngnhatnam1510@gmail.com>
2024-09-18 11:39:49 +0700

    create user model

--- api/index.js
+++ api/index.js
@@ -1,23 +1,22 @@
 // const express = require('express')
-import express from 'express'
-import mongoose from 'mongoose';
-import dotenv from "dotenv"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-mongoose.connect(process.env.MONGO_DB)
+mongoose
+  .connect(process.env.MONGO_DB)
 
-    .then(() => {
-        console.log("Connect MongoDB successfully");
-
-    })
-    .catch((error) => {
-        console.log("Error connecting to DB", error);
-    });
+  .then(() => {
+    console.log("Connect MongoDB successfully");
+  })
+  .catch((error) => {
+    console.log("Error connecting to DB", error);
+  });
 
 const app = express();
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log("server running on", port);
-
-})
+  console.log("server running on", port);
+});
