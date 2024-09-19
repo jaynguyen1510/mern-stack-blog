b68b2ec2b45ee389fc35b1afa1888eb8713f8d14
jaynguyen1510 <ngnhatnam1510@gmail.com>
2024-09-19 14:34:17 +0700

    fix-new

--- api/index.js
+++ api/index.js
@@ -20,6 +20,16 @@ app.use(
 
 app.use(express.json());
 
+app.use((error, req, res, next) => {
+  const status = error.status || 400;
+  const message = error.message || "Internal Server Error";
+  res.status(status).json({
+    status: status,
+    success: false,
+    message,
+  });
+});
+
 app.use(cors());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
