f4f6f6dfa50d096f4588b751f0111aed2998801b
jaynguyen1510 <ngnhatnam1510@gmail.com>
2025-03-10 21:41:07 +0700

    complete about and project page

--- client_FE/src/pages/About.jsx
+++ client_FE/src/pages/About.jsx
@@ -1,5 +1,27 @@
 const About = () => {
-    return <div>About</div>;
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="max-w-2xl mx-auto p-3 text-center">
+                <div className="">
+                    <h1 className="text-3xl font font-semibold text-center my-7">
+                        Giới thiệu trang web là nơi chia sẻ kiến thức, kinh nghiệm và cảm hứng về thể hình, dinh dưỡng
+                        và lối sống lành mạnh
+                    </h1>
+                    <div className="text-md text-gray-500 flex flex-col gap-6">
+                        <p>
+                            Chào mừng bạn đến với Fitness and Blog – nơi hội tụ đam mê thể hình, dinh dưỡng và phong
+                            cách sống lành mạnh. Chúng tôi mang đến cho bạn những bài viết chất lượng về bài tập, chế độ
+                            ăn uống cân bằng, và những câu chuyện truyền cảm hứng từ các chuyên gia hàng đầu. Mục tiêu
+                            của chúng tôi là hỗ trợ bạn khám phá và phát triển sức khỏe toàn diện, biến mỗi ngày trở
+                            thành một hành trình tự chăm sóc bản thân hiệu quả và ý nghĩa. Hãy cùng tham gia cộng đồng
+                            của chúng tôi để cập nhật những xu hướng tập luyện mới nhất và những bí quyết duy trì phong
+                            độ tốt mỗi ngày!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default About;
--- client_FE/src/pages/ProjectPage.jsx
+++ client_FE/src/pages/ProjectPage.jsx
@@ -1,5 +1,20 @@
+import CallToAction from '../components/CallToAction/CallToAction';
+
 const ProjectPage = () => {
-    return <div>ProjectPage</div>;
+    return (
+        <div className="container mx-auto p-6">
+            {/* Tiêu đề */}
+            <h1 className="text-4xl font-bold mb-4 text-center">Lịch Tập</h1>
+
+            {/* Mô tả giới thiệu */}
+            <p className="text-lg text-gray-700 mb-8 text-center">
+                Trang Lịch Tập của chúng tôi cung cấp kế hoạch tập luyện chi tiết, giúp bạn theo dõi và cải thiện sức
+                khỏe hàng ngày. Tại đây, bạn có thể xem lịch biểu tập hàng tuần, cập nhật tiến độ và nhận những lời
+                khuyên bổ ích từ các chuyên gia.
+            </p>
+            <CallToAction />
+        </div>
+    );
 };
 
 export default ProjectPage;
